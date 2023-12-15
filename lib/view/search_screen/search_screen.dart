1ada6ce6b51f17a6e6ec28735c8f7b6ab58f6de6
muhammed-aslam-pm <muhammedaslampm707@gmail.com>
2023-12-15 21:33:51 +0530

    animation changed

--- lib/view/search_screen/search_screen.dart
+++ lib/view/search_screen/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:news_app_with_api/controller/search_screen_controller.dart';
 import 'package:news_app_with_api/view/widgets/news_card.dart';
 import 'package:provider/provider.dart';
@@ -25,62 +26,75 @@ class _SearchScreenState extends State<SearchScreen> {
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.all(20),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    height: 45,
-                    width: MediaQuery.sizeOf(context).width * 2 / 3,
-                    child: TextField(
-                      controller: textController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide:
-                                BorderSide(color: Color(0xff6A3DE8), width: 3)),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  height: 45,
+                  width: MediaQuery.sizeOf(context).width * 2 / 3,
+                  child: TextField(
+                    controller: textController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              BorderSide(color: Color(0xff6A3DE8), width: 3)),
                     ),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      provider.searchData(
-                          searchText: textController.text.toLowerCase());
-                      FocusManager.instance.primaryFocus?.unfocus();
-                    },
-                    child: Text("Search"),
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStatePropertyAll(Color(0xff6A3DE8))),
-                  )
-                ],
-              ),
-              ListView.separated(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemBuilder: (context, index) => NewsCard(
-                      title: provider.newsModel?.articles?[index].title ?? "",
-                      description:
-                          provider.newsModel?.articles?[index].description ??
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    provider.searchData(
+                        searchText: textController.text.toLowerCase());
+                    FocusManager.instance.primaryFocus?.unfocus();
+                  },
+                  child: Text("Search"),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll(Color(0xff6A3DE8))),
+                )
+              ],
+            ),
+            Expanded(
+              child: Provider.of<SearchScreenController>(context).isLoading ==
+                      true
+                  ? Center(
+                      child: LottieBuilder.asset(
+                      "assets/animations/Animation - 1702395258490 (2).json",
+                      fit: BoxFit.cover,
+                      height: 150,
+                      width: 150,
+                    ))
+                  : ListView.separated(
+                      itemBuilder: (context, index) => NewsCard(
+                          title:
+                              provider.newsModel?.articles?[index].title ?? "",
+                          description:
+                              provider.newsModel?.articles?[index].description ??
+                                  "",
+                          date:
+                              provider.newsModel?.articles?[index].publishedAt,
+                          imageUrl:
+                              provider.newsModel?.articles?[index].urlToImage ??
+                                  "",
+                          contant: provider.newsModel?.articles?[index].content ??
                               "",
-                      date: provider.newsModel?.articles?[index].publishedAt,
-                      imageUrl:
-                          provider.newsModel?.articles?[index].urlToImage ?? "",
-                      contant:
-                          provider.newsModel?.articles?[index].content ?? "",
-                      sourceName:
-                          provider.newsModel?.articles?[index].source?.name ??
-                              "",
-                      url: provider.newsModel?.articles?[index].url ?? ""),
-                  separatorBuilder: (context, index) => Divider(height: 20),
-                  itemCount: provider.newsModel?.articles?.length ?? 0)
-            ],
-          ),
+                          sourceName:
+                              provider.newsModel?.articles?[index].source?.name ??
+                                  "",
+                          url: provider.newsModel?.articles?[index].url ?? ""),
+                      separatorBuilder: (context, index) => Divider(height: 20),
+                      itemCount: provider.newsModel?.articles?.length ?? 0),
+            )
+          ],
         ),
       )),
     );
